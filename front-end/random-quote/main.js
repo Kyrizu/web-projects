a0401964d70e399ab18131632a80f3bb534c8de1
Kyrizu <hydragon61@gmail.com>
2017-12-02 14:15:14 +1100

    Update main.js

--- front-end/random-quote/main.js
+++ front-end/random-quote/main.js
@@ -1,9 +1,10 @@
+
 $(document).ready(function() {
     $("#gen").on("click", function() {
       $.getJSON("quote.json", function(json) {
         
-        var index = Math.random()*json.length;
-        var quote = json[index]["quote"];
+        var index = Math.floor(Math.random()*json.length);
+        var quote = "\""+json[index]["quote"]+"\"";
         var author = "-"+json[index]["author"];
         
         $("#quote").html(quote);
