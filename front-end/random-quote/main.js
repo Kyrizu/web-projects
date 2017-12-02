ce341cd52ba2afff0d3c1cec146d8a1ce4d300ce
Kyrizu <hydragon61@gmail.com>
2017-12-02 17:42:48 +1100

    added fading effect & tweeter btn

--- front-end/random-quote/main.js
+++ front-end/random-quote/main.js
@@ -7,8 +7,16 @@ $(document).ready(function() {
         var quote = "\""+json[index]["quote"]+"\"";
         var author = "-"+json[index]["author"];
         
-        $("#quote").html(quote);
-        $("#author").html(author);
+        var tweet = quote + author;
+        var hashtag = "quotes";
+        var related = "freecodecamp";
+        
+        $("#tweet").attr('href', 'https://twitter.com/intent/tweet?hashtags='+hashtag+'&related='+related+'&text=' + window.encodeURIComponent(quote+" "+author));
+        $("#quote, #author").fadeOut(function() {
+          $("#quote").html(quote).fadeIn(750);
+          $("#author").html(author).fadeIn(750);
+        });
+          
       });
     });
   });
